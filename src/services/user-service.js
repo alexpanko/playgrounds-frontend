fea6a0a2ca7b998da14aac889598c724029a1ae3
Alex Panko <alexander.panko@gmail.com>
2020-06-20 15:46:18 +0200

    update service and create _redirects for Netlify

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -7,9 +7,8 @@ import { toast } from 'react-toastify';
 class UserService {
   constructor() {
     this.service = axios.create({
-      baseURL: 'https://playgrounds-in-amsterdam.herokuapp.com/',
-      // process.env.REACT_APP_BASE_URL,
-      //'http://localhost:4000'
+      // baseURL: 'http://localhost:4000/',
+      baseURL: process.env.REACT_APP_BASE_URL,
       withCredentials: true
     })
     //this.service = service
